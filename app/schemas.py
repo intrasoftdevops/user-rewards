b446644f29775d18a2603a5655c46aa35ebd4c9c
Alvaro Campos <alvarojosecampos64lol@gmail.com>
2025-08-19 19:58:42 -0500

    V1 endPoints

---
+++ app/schemas.py
@@ -0,0 +1,79 @@
+from pydantic import BaseModel
+from typing import List, Dict
+from datetime import datetime
+from pydantic import BaseModel, Field
+from datetime import datetime
+from typing import Optional
+
+
+
+class User(BaseModel):
+    user_id: str
+    email: str
+    name: str
+
+class UsersResponse(BaseModel):
+    users: List[User]
+    count: int
+
+class UserPoints(BaseModel):
+    user_id: str
+    points: int
+    last_updated: datetime
+
+class UserPointsResponse(BaseModel):
+    success: bool
+    data: UserPoints
+    message: str = "Puntos obtenidos exitosamente"   
+
+#####################################
+class ChallengeCreate(BaseModel):
+    name: str = Field(..., min_length=3, max_length=50)
+    description: str = Field(..., min_length=10, max_length=500)
+    max_limit: int = Field(..., gt=0)
+    reward_id: str = Field(..., min_length=1)
+    max_users: int = Field(..., gt=0)
+    status: Optional[str] = Field("active", pattern="^(active|inactive|completed)$")  # ¡Cambiado aquí!
+    max_date: Optional[datetime] = None
+
+class ChallengeResponse(ChallengeCreate):
+    challenge_id: str
+    date_creation: datetime
+##################################### en listar challenges 
+class ChallengesResponse(BaseModel):
+    success: bool
+    challenges: List[ChallengeResponse]
+    count: int
+
+######################################
+
+class RewardCreate(BaseModel):
+    type: str = Field(..., min_length=1, description="Tipo de recompensa (ej. 'points', 'badge', 'item')")
+    value: str = Field(..., min_length=1, description="Valor de la recompensa")
+    metadata: Optional[Dict] = Field(None, description="Metadatos adicionales")
+
+class RewardResponse(RewardCreate):
+    reward_id: str
+    created_at: datetime
+
+
+######################################
+
+class ChallengeInstanceCreate(BaseModel):
+    user_id: str = Field(..., min_length=1, description="ID del usuario")
+    challenge_id: str = Field(..., min_length=1, description="ID del challenge")
+
+class ChallengeInstanceResponse(ChallengeInstanceCreate):
+    instance_id: str
+    progress: int = 0
+    completed: bool = False
+    date_started: datetime
+
+
+###################################
+
+class UserAssignedChallengesResponse(BaseModel):
+    success: bool
+    user_id: str
+    challenges: List[dict]
+    count: int
